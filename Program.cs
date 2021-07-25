8b97272e352109a3f941e8d381f710b2e1caf465
arpita-simpi <87927523+arpita-simpi@users.noreply.github.com>
2021-07-25 23:47:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace visualstudiodemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello,world");
+            Console.ReadLine();
+        }
+    }
+}
